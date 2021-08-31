4c39f57378ee19d156ee15f234931e5709910684
Ayush srivastava <ayush.sriva6@gmail.com>
2021-08-31 23:57:32 +0530

    up

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Center(
+        child: MaterialButton(
+          onPressed: () async {
+            FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+            if (result != null) {
+              PlatformFile file = result.files.first;
+
+              print(file.name);
+              print(file.bytes);
+              print(file.size);
+              print(file.extension);
+              print(file.path);
+            } else {
+              // User canceled the picker
+            }
+          },
+          child: Text('click to upload'),
+        ),
+      ),
+    );
+  }
+}
